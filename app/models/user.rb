cd4c2ceb8cb8ce6f5df18be2ece1fc0035a23a0f
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-07-15 14:09:58 -0500

    Update user.rb
    - created relationship & queries

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: :author_id
+  has_many :comments, foreign_key: :author_id
+  has_many :likes, foreign_key: :author_id
+
+  def find_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
+
+  def find_first_post
+    posts.order(created_at: :asc).limit(1)
+  end
 end
