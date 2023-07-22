cd87a4812eec8443f52551a02d0f4a1a4a1eb8a5
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-07-22 17:51:50 -0500

    Stablished Controller
    - stablished the requested routes
    - stablished views for the routes
    - checked linters
    - checked that all routes worked properly
    - re-established database

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,9 @@ class Comment < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   belongs_to :post, foreign_key: :post_id
 
+  after_create :add_count # after create, increment post count
+  after_destroy :remove_count # after destroy, decrement post count
+
   def add_count
     post.increment!(:comments_counter)
   end
