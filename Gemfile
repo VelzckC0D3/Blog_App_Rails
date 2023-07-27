3c9a3b6ab168598a72239fc30d2cd011345e0101
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-07-27 14:22:31 -0500

    set up devise
    - set up devise gem

--- Gemfile
+++ Gemfile
@@ -79,3 +79,5 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
+gem "devise", "~> 4.9"
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,11 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+  # Set mailer delivery method
   config.action_mailer.perform_caching = false
+
+  # Set mailer default url
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } #ADDED
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
