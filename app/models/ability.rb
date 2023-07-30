0b09ac6b0c7813da7b89c6a9e924d90aedc3cc3b
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-07-29 20:08:12 -0500

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,6 +6,7 @@ class Ability
     can :create, :user
     can :manage, :all if user.role == 'admin'
     return unless user.role == 'user'
+    
     can :read, :all
     can :create, Post, author_id: user.id
     can :create, Comment, author_id: user.id
