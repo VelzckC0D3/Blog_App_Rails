b1502bb175d599868d653f138c92031525f06d9e
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-07-29 20:06:13 -0500

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,20 +1,15 @@
 class Ability
   include CanCan::Ability
-
   def initialize(user)
     user ||= User.new
     can :read, :all
     can :create, :user
-
     can :manage, :all if user.role == 'admin'
-
     return unless user.role == 'user'
-
     can :read, :all
     can :create, Post, author_id: user.id
     can :create, Comment, author_id: user.id
     can :destroy, Comment, author_id: user.id
     can :destroy, Post, author_id: user.id
-
   end
 end
